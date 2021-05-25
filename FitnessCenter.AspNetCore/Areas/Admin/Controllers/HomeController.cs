82feefcf5346ee0613be2ee1cb1ed22dfcac426b
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-05-25 11:04:03 +0500

    Произведена базовая настройка приложения.

---
+++ FitnessCenter.AspNetCore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenter.AspNetCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        [Route("~/Admin")]
+        [Route("~/Admin/Home")]
+        [Route("~/Admin/Home/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
