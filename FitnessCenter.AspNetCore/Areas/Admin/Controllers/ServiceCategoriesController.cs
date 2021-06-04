8e005cd58fa9e3223af72e70ff3e062721bb7e20
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-06-04 14:25:50 +0500

    Написан функционал админ панели для сущности "ServiceCategory".

---
+++ FitnessCenter.AspNetCore/Areas/Admin/Controllers/ServiceCategoriesController.cs
@@ -0,0 +1,67 @@
+﻿using FitnessCenter.AspNetCore.Models;
+using FitnessCenter.Model.Database;
+using FitnessCenter.Model.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace FitnessCenter.AspNetCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceCategoriesController : Controller
+    {
+        private readonly DataManager _dataManager;
+
+        public ServiceCategoriesController(DataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+        [Route("~/Admin/ServiceCategories")]
+        [Route("~/Admin/ServiceCategories/Index")]
+        public IActionResult Index()
+        {
+            var viewModel = new ServiceCategoriesViewModel()
+            {
+                ServiceCategories = _dataManager.ServiceCategories.GetServiceCategories().ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Save()
+        {
+            return View(new ServiceCategory());
+        }
+
+        [Route("~/Admin/ServiceCategories/Save/{id}")]
+        public IActionResult Save(Guid id)
+        {
+            return View(_dataManager.ServiceCategories.GetServiceCategoryById(id));
+        }
+
+        [HttpPost]
+        public IActionResult Save(ServiceCategory serviceCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_dataManager.ServiceCategories.SaveServiceCategory(serviceCategory))
+                {
+                    return RedirectToAction("Index");
+                }
+
+                ModelState.AddModelError(nameof(serviceCategory.Name), "Неверное наименование");
+            }
+
+            return View(serviceCategory);
+        }
+
+        [Route("~/Admin/ServiceCategories/Delete/{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            _dataManager.ServiceCategories.DeleteServiceCategoryById(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FitnessCenter.AspNetCore/Models/ServiceCategoriesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FitnessCenter.Model.Database.Entities;
+using System.Collections.Generic;
+
+namespace FitnessCenter.AspNetCore.Models
+{
+    public class ServiceCategoriesViewModel
+    {
+        public List<ServiceCategory> ServiceCategories { get; set; }
+    }
+}
