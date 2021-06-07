f7112fb60064b88a01e824a7cbd42c198034b778
Lev4and <54065747+Lev4and@users.noreply.github.com>
2021-06-07 11:49:19 +0500

    Улучшение панели администратора.

---
+++ FitnessCenter.AspNetCore/Areas/Admin/Components/UserProfile.cs
@@ -0,0 +1,33 @@
+﻿using FitnessCenter.AspNetCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FitnessCenter.AspNetCore.Areas.Admin.Components
+{
+    public class UserProfile : ViewComponent
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserProfile(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //var user = _userManager.FindByNameAsync(User.Identity.Name);
+            //var role = _userManager.GetRolesAsync(user.Result);
+
+            var viewModel = new UserProfileViewModel()
+            {
+                //Name = user.Result.UserName,
+                //Role = role.Result.First()
+                Name = "",
+                Role = ""
+            };
+
+            return View("Default", viewModel);
+        }
+    }
+}
---
+++ FitnessCenter.AspNetCore/Models/UserProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FitnessCenter.AspNetCore.Models
+{
+    public class UserProfileViewModel
+    {
+        public string Name { get; set; }
+
+        public string Role { get; set; }
+    }
+}
